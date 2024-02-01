c923f93d7a461856140055fbe16ad53742e6bea5
prangchira <prangchira@gmail.com>
2024-02-01 20:20:42 +0000

    Add API fetch for themoviedb

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+
+var queryUrl= `https://api.themoviedb.org/3/search/movie?query=Garlic&api_key=b72e96c54ea2a07a5e26896ddd3161a7`
+  fetch(queryUrl).then(function (response) {
+      return response.json();
+  }).then(function (data) {
+console.log(data)
+    })
