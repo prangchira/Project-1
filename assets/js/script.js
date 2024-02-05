24d3cd1eb1f54ed0bde18b9bd6afeb4278d89737
Steve Hannay <hannay.steve@googlemail.com>
2024-02-05 13:29:08 +0000

    addition of API calls and population of variables

---
+++ assets/js/script.js
@@ -0,0 +1,357 @@
+/* 
+    Javascript for Project 1
+    Developers - Team 8 
+    First Created - 1st February 2024
+
+    This code handles logic for the "Movies for Munchies" application
+
+*/
+
+
+// Event Listeners
+
+// - "Search" button "click" event
+$("#search-btn").on('click', function(event){
+  // start a new search when the user clicks on the search button
+  event.preventDefault()
+  console.log("**** Starting New Search ****")
+  searchForMealAndMovies() 
+})
+
+
+// Variable Declarations
+// notes : The following variables are placed here so that they are in scope for all functions
+
+// - API Calling
+var apiURL_Food = "https://www.themealdb.com/api/json/v1/1/search.php"   
+var apiURL_Movie = "https://api.themoviedb.org/3/search/movie"
+var apiKey_Movie  = "b72e96c54ea2a07a5e26896ddd3161a7"    
+
+// USER Input
+var userInput_NameOfMeal = ""
+
+// FOOD API 
+// note : the object foodDetails is populated with the meal information returned from the Food API
+var foodDetails = {
+  Meal : "",          
+  Catagory : "",
+  Area: "",
+  MealThumb : "",         // image
+  Ingredients : [],       // array of ingredients
+  Source : "",
+  YouTube : ""
+}
+
+// MOVIE API
+// notes : 
+// The object movieDetails is populated with each movie returned from the Movie API
+// It is then added to the moviesList array
+var movieDetails = {
+  Title : "",
+  OriginalTitle : "", 
+  OriginalLanguage : "",       
+  Overview : "",
+  Popularity : "",
+  BackdropPath : "",      // image
+  PosterPath : "",        // image
+  ReleaseDate : ""
+  }
+
+var moviesList = []         // an array of movies returned from the Movie API
+
+
+
+
+
+// MAIN LOGIC 
+
+// Do Search - called when the user clicks on the "Search" button
+function searchForMealAndMovies() {
+
+  // Notes : 
+  // By breaking tasks down into individual functions, team members can more easily be assigned to complete them.
+
+
+  // (1) Get User Input and Initialise application
+  // ---------------------------------------------
+
+  // Reset variables to their initial values
+  initialiseVariables()
+
+  // Get User Input, Validate User Input and Populate the userInput_NameOfMeal variable
+  getUserInputAndValidate()
+
+  // Reset screen (clear any existing search results currently displayed on screen) 
+  resetScreen()
+
+
+  // (2) Get information from "Food API"
+  // -----------------------------------
+
+  // Search "Food API" for the Meal that the user entered and Populate foodAPI variables with results
+  searchFoodAPI()
+
+
+
+  // this code will run after 2 seconds ***TEMPORY CODE ****
+  setTimeout(() => {
+
+
+  // If the "Food API" returned No Data, let the user know and STOP Search
+  validateFoodAPIData()
+
+  // Display the results from the "Food API" (update screen using foodAPI variables)
+  displayResultsFromFoodAPI()
+
+
+  // (2) Get information from "Movie API"
+  // ------------------------------------
+
+  // REPEAT for items of information returned from the "Food API"                   
+  
+      // Search "Movie API" and Populate movieList variables with results
+      searchMovieAPI(foodDetails.Meal)
+
+  // Display the results from the "Movie API" (update screen using movieList variables)
+  displayResultsFromMovieAPI()
+
+
+  }, 2000) // 2 second delay ******** 
+
+
+}
+
+
+
+// SUPPORTING FUNCTIONS
+
+
+// Reset variables to their initial values
+function initialiseVariables() {
+
+  // user Input
+  userInput_NameOfMeal = ""
+
+  // food Details
+  foodDetails.Meal = ""    
+  foodDetails.Catagory = ""
+  foodDetails.Area = ""
+  foodDetails.MealThumb  = ""
+  foodDetails.Ingredients = []   
+  foodDetails.Source = ""
+  foodDetails.YouTube = ""
+
+  // movie Details and movies List
+  movieDetails.Title = "",
+  movieDetails.OriginalTitle = "", 
+  movieDetails.OriginalLanguage = "",       
+  movieDetails.Overview = "",
+  movieDetails.Popularity = "",
+  movieDetails.BackdropPath = "",
+  movieDetails.PosterPath = "",
+  movieDetails.ReleaseDate = ""
+  
+  moviesList = []     // empty the list of movies
+
+}
+
+
+// Get User Input, Validate User Input and Populate Variable
+function getUserInputAndValidate() {
+
+  // get user input from the webpage
+  userInput_NameOfMeal  = $("#search-input").val().trim()
+
+  // if the user has not entered anything at all, display an error message (eg. "please enter a meal")
+  if (userInput_NameOfMeal === "") {
+      alert("Please enter the name of a Meal")   // **** WE NEED TO REPLACE THIS, BECAUSE WE ARE NOT SUPPOSSED TO USE ALERTS 
+  }
+  else {
+      console.log("User entered '" + userInput_NameOfMeal + "'")
+  }
+}
+
+
+// Reset webpage (clear any existing search results)
+function resetScreen() {
+  // clear any existing search results (food and movies) from the WebPage
+
+}
+
+
+// Search "Food API" for the Meal that user entered and Populate food API variables with results
+function searchFoodAPI() {
+
+  // build API query for Food
+  var queryURL = apiURL_Food + "?s=" + userInput_NameOfMeal
+  console.log("Food queryURL = " + queryURL)
+  
+  // run API query
+  fetch(queryURL).then(function (response) {
+      return response.json()
+  }).then(function (data) {
+
+      // Check if a Meal was found
+      if (data.meals === null) {
+          console.log("Meal NOT Found by API")
+
+      }
+      else {
+          console.log("Meal Found by API")
+
+          // populate the variable (object) foodDetails with data returned from API
+          foodDetails.Meal = data.meals[0].strMeal        
+          foodDetails.Catagory = data.meals[0].strCategory
+          foodDetails.Area = data.meals[0].strArea
+          foodDetails.MealThumb = data.meals[0].strMealThumb
+          foodDetails.Source = data.meals[0].strSource
+          foodDetails.YouTube = data.meals[0].strYoutube
+
+          if (data.meals[0].strIngredient1 != "") {foodDetails.Ingredients.push(data.meals[0].strIngredient1)} // if not empty string, add to Ingredients array
+          if (data.meals[0].strIngredient2 != "") {foodDetails.Ingredients.push(data.meals[0].strIngredient2)}
+          if (data.meals[0].strIngredient3 != "") {foodDetails.Ingredients.push(data.meals[0].strIngredient3)}
+          if (data.meals[0].strIngredient4 != "") {foodDetails.Ingredients.push(data.meals[0].strIngredient4)}
+          if (data.meals[0].strIngredient5 != "") {foodDetails.Ingredients.push(data.meals[0].strIngredient5)}
+          if (data.meals[0].strIngredient6 != "") {foodDetails.Ingredients.push(data.meals[0].strIngredient6)}
+          if (data.meals[0].strIngredient7 != "") {foodDetails.Ingredients.push(data.meals[0].strIngredient7)}
+          if (data.meals[0].strIngredient8 != "") {foodDetails.Ingredients.push(data.meals[0].strIngredient8)}
+          if (data.meals[0].strIngredient9 != "") {foodDetails.Ingredients.push(data.meals[0].strIngredient9)}
+          if (data.meals[0].strIngredient10 != "") {foodDetails.Ingredients.push(data.meals[0].strIngredient10)}
+
+          console.log(foodDetails)
+
+      } 
+
+  }) // end of Food API fetch
+  
+}
+
+
+// If the "Food API" returned No Data, let the user know 
+function validateFoodAPIData() {
+  // if the variable foodDetails contains no values, let the user know 
+  if (foodDetails.Meal === "") {
+      // display user message on webpage 
+
+  }  
+}
+
+
+// Display the results from the "Food API" (update screen using foodAPI variables)
+function displayResultsFromFoodAPI() {
+
+  // populate the webpage with the values from the variable foodDetails
+
+  // eg. 
+  // webpage element = foodDetails.Meal
+  // webpage element = foodDetails.MealThumb 
+  // etc.
+
+
+
+}
+
+
+// Search "Movie API" and Populate movieList variable with results
+function searchMovieAPI(searchString) {
+
+  // build API query for Movies
+  var queryString = searchString.split(' ').join('_') // remove spaces
+
+  var queryURL= apiURL_Movie + "?query=" + queryString + "&api_key=" + apiKey_Movie
+  console.log("Movie queryURL = " + queryURL)
+
+  // run API query
+  fetch(queryURL).then(function (response) {
+      return response.json()
+  }).then(function (data) {
+
+      // Check if a Movies were found
+      if (data.results === null) {
+          console.log("Movies NOT Found by API")
+
+      }
+      else {
+          console.log("Movies Found by API")
+      }
+
+
+      // For each movie returned by the Movie API
+      for (i=0; i< data.results.length; i++) {
+
+          if (data.results[i].adult === false) {  // filter out adult movies 
+
+              // save the movie Details to the variable (object) movieDetails
+              movieDetails.Title = data.results[i].title
+              movieDetails.OriginalTitle = data.results[i].original_title
+              movieDetails.OriginalLanguage = data.results[i].original_language
+              movieDetails.Overview = data.results[i].overview
+              movieDetails.Popularity = data.results[i].popularity
+              movieDetails.BackdropPath = data.results[i].backdrop_path
+              movieDetails.PosterPath = data.results[i].poster_path
+              movieDetails.ReleaseDate = data.results[i].release_date
+
+              // add the movie details to the movies List array
+              // notes : 
+              // A clone of the movieDetails is appended to the array (so that we are adding a string value and not an object reference)
+
+              let clone = JSON.parse(JSON.stringify(movieDetails))
+
+              moviesList.push(clone)
+
+          }
+
+      }
+
+      // sort the movies List by Popularity (descending order)
+      moviesList.sort((a,b) => b.Popularity - a.Popularity)
+
+
+    }) // end of Movie API fetch
+
+}
+
+
+// Display the results from the "Movie API" (update WebPage using moviesList variable)
+function displayResultsFromMovieAPI() {
+
+
+  // this code will run after 2 seconds ***TEMPORY CODE ****
+  setTimeout(() => {
+
+
+  console.log("Movies List :")
+  console.log(moviesList)
+
+  console.log("Top Movie :")
+  console.log(moviesList[0])
+
+
+  // display the first Movie in the list onto the WebPage
+      
+  // eg. 
+  // webpage element = moviesList[0].Title
+  // webpage element = moviesList[0].release_date 
+  // etc.
+  
+
+  }, 2000) // 2 second delay ******** 
+
+}
+
+
+
+
+
+
+
+
+
+// TEST code
+
+init()  // this should run when index.html opens
+function init() {
+  console.log("HTML reference to Javascript is ok")
+}
+
+
